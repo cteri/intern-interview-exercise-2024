ec58dc4431c7d6a9be0a8e3e3ff76d8ad639a7d8
cteri <terri98c@gmail.com>
2024-03-21 09:11:29 -0400

    Remove unused code

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/search', function () {
     return view('search');
 });
 
